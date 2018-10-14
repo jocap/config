2ff358e887dfc793b864a43deeef301365b04ada
John Ankarström <john@ankarstrom.se>
2018-10-14 17:49:52 +0200

    vis: added plugin vis-filetype-settings

---
+++ vis/plugins/vis-filetype-settings.lua
@@ -0,0 +1,1 @@
+/home/john/dev/config/vis/plugins/vis-filetype-settings/vis-filetype-settings.lua
--- vis/visrc.lua
+++ vis/visrc.lua
@@ -1,19 +1,24 @@
-require('vis')
-require('plugins/vis-modelines')
+require("vis")
+require("plugins/vis-modelines")
+require("plugins/vis-filetype-settings")
+	
+settings = {
+	markdown = {"set tw 4", "set et on"}
+}
 
 vis.events.subscribe(vis.events.INIT, function()
-	vis:command('set theme acme')
-	vis:command('set autoindent on')
-	vis:command('set shell /bin/sh')
-	vis:command('map! normal W :w<Enter>')
+	vis:command("set theme acme")
+	vis:command("set autoindent on")
+	vis:command("set shell /bin/sh")
+	vis:command("map! normal W :w<Enter>")
 
-	vis:command('set expandtab off')
-	vis:command('set tabwidth 4')
+	vis:command("set expandtab off")
+	vis:command("set tabwidth 4")
 end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
-	vis:command('set savemethod inplace')
-	vis:command('set relative')
+	vis:command("set savemethod inplace")
+	vis:command("set relative")
 end)
 
 vis.ftdetect.filetypes.bash.mime = {
