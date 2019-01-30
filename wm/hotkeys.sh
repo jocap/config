0c968cb1139db9c8af687989d99e21fbacbff8e3
John <john@ankarstrom.se>
2019-01-31 00:00:52 +0100

    bspwm: changed hotkeys.sh to use bash
    (i haven't installed mksh on my new computer)

--- wm/hotkeys.sh
+++ wm/hotkeys.sh
@@ -1,2 +1,5 @@
-#!/bin/mksh
+#!/usr/bin/env bash
+
+# (for some reason, /bin/sh doesn't work)
+
 cat /home/john/dev/config/wm/sxhkdrc | awk '/^[a-z]/ && last {print $0,"\t",last} {last=""} /^#/{last=$0}' | column -t -s $'\t' | fzf
