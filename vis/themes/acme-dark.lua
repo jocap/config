9ff96babcd207a110ca18327bdeb75d2269aa60e
John <john@ankarstrom.se>
2019-02-25 12:15:50 +0100

    x11/vis: change to gray color scheme

--- vis/themes/acme-dark.lua
+++ vis/themes/acme-dark.lua
@@ -1,10 +1,10 @@
 -- Acme editor color scheme
 local lexers = vis.lexers
 
-lexers.STYLE_DEFAULT = '' -- ',back:#1c1c1c'
+lexers.STYLE_DEFAULT = ''
 lexers.STYLE_NOTHING = ''
 lexers.STYLE_CLASS = ''
-lexers.STYLE_COMMENT = 'fore:#a8a8a8'
+lexers.STYLE_COMMENT = 'fore:#7F8D9E' -- #a8a8a8
 lexers.STYLE_CONSTANT = ''
 lexers.STYLE_DEFINITION = ''
 lexers.STYLE_ERROR = 'fore:red'
@@ -12,7 +12,7 @@ lexers.STYLE_FUNCTION = ''
 lexers.STYLE_KEYWORD = ',bold'
 lexers.STYLE_LABEL = ''
 lexers.STYLE_NUMBER = ''
-lexers.STYLE_OPERATOR = 'fore:#a8a8a8'
+lexers.STYLE_OPERATOR = 'fore:#7F8D9E'
 lexers.STYLE_REGEX = ''
 lexers.STYLE_STRING = 'fore:white'
 lexers.STYLE_PREPROCESSOR = 'fore:white'
@@ -23,15 +23,15 @@ lexers.STYLE_WHITESPACE = ''
 lexers.STYLE_EMBEDDED = 'back:#666666'
 lexers.STYLE_IDENTIFIER = ''
 
-lexers.STYLE_LINENUMBER = ''
-lexers.STYLE_LINENUMBER_CURSOR = lexers.STYLE_LINENUMBER
+lexers.STYLE_LINENUMBER = 'fore:#666666'
+lexers.STYLE_LINENUMBER_CURSOR = 'fore:default'
 lexers.STYLE_CURSOR = 'reverse'
 lexers.STYLE_CURSOR_PRIMARY = lexers.STYLE_CURSOR..',fore:#90ece9'
 lexers.STYLE_CURSOR_LINE = 'underlined'
 lexers.STYLE_COLOR_COLUMN = 'back:blue'
-lexers.STYLE_SELECTION = 'back:#444444'
-lexers.STYLE_STATUS = 'back:blue'
-lexers.STYLE_STATUS_FOCUSED = 'back:blue,bold'
+lexers.STYLE_SELECTION = 'back:#4E698C' -- #444444
+lexers.STYLE_STATUS = 'back:#4E698C'
+lexers.STYLE_STATUS_FOCUSED = 'back:#4E698C,bold'
 lexers.STYLE_SEPARATOR = lexers.STYLE_DEFAULT
 lexers.STYLE_INFO = 'fore:default,back:default,bold'
-lexers.STYLE_EOF = ''
+lexers.STYLE_EOF = 'fore:#444444'
