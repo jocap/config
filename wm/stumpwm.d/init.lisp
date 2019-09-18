5675914850f33610739cda39b82d1488c434e13c
John <john@ankarstrom.se>
2019-09-18 22:14:33 +0200

    wip -- keep all or not?

--- wm/stumpwm.d/init.lisp
+++ wm/stumpwm.d/init.lisp
@@ -2,13 +2,16 @@
 
 (in-package :stumpwm)
 
+(export '(*autosplit-next* current-frame))
+
 ;; Key bindings
 
 (set-prefix-key (kbd "C-t"))
 
-(define-key *root-map* (kbd "c") "exec u")
+(define-key *root-map* (kbd "c") "exec t")
 (define-key *root-map* (kbd "C") "exec urxvtc")
 (define-key *root-map* (kbd "M-b") "battery-status")
+(define-key *root-map* (kbd "d") "exec dwim")
 
 (define-key *top-map* (kbd "M-TAB") "fnext")
 (define-key *top-map* (kbd "M-ISO_Left_Tab") "fprevious")
@@ -18,37 +21,44 @@
 (define-key *top-map* (kbd "s-TAB") "gnext")
 (define-key *top-map* (kbd "s-ISO_Left_Tab") "gprev")
 
-(define-key *top-map* (kbd "s-k") "move-focus up")
-(define-key *top-map* (kbd "s-j") "move-focus down")
-(define-key *top-map* (kbd "s-h") "move-focus left")
-(define-key *top-map* (kbd "s-l") "move-focus right")
-
 (define-key *root-map* (kbd "k") "move-focus up")
 (define-key *root-map* (kbd "j") "move-focus down")
 (define-key *root-map* (kbd "h") "move-focus left")
 (define-key *root-map* (kbd "l") "move-focus right")
 
-(define-key *top-map* (kbd "s-K") "move-window up")
-(define-key *top-map* (kbd "s-J") "move-window down")
-(define-key *top-map* (kbd "s-H") "move-window left")
-(define-key *top-map* (kbd "s-L") "move-window right")
+(define-key *root-map* (kbd "M-k") "move-with-window up")
+(define-key *root-map* (kbd "M-j") "move-with-window down")
+(define-key *root-map* (kbd "M-h") "move-with-window left")
+(define-key *root-map* (kbd "M-l") "move-with-window right")
 
 (define-key *root-map* (kbd "H") "*help-map*") ; TODO: make this work
 
 (define-key *exchange-window-map* (kbd "l") "exchange-direction right")
 (define-key *exchange-window-map* (kbd "h") "exchange-direction left")
 
+(define-key *root-map* (kbd "SPC") "autosplit-next")
+
 (define-key *root-map* (kbd "C-r") "loadrc")
 (define-key *root-map* (kbd "/") "mode-line")
+
+(define-key *top-map* (kbd "F1") "setlang usx")
+(define-key *top-map* (kbd "F2") "setlang sex")
+(define-key *top-map* (kbd "F3") "setlang rux")
 
 ;; Settings
 
 (setf *screen-mode-line-format*
-  (list '(:eval (check-battery))
-        " [%n] %W"))
+  (list ; '(:eval (check-battery)) " "
+        "[%n] %W"
+        ; "^>" ; right align
+        ; '(:eval (check-lang))
+        ))
 (setf *mode-line-position* :top)
+(setf *mode-line-foreground-color* "#d0d0d0")
 
 (setf *timeout-wait* 999)
+(setf *mouse-focus-policy* :sloppy) ; focus follows mouse
+(setf *suppress-frame-indicator* t) ; required for automovemouse-hook
 
 (setf *window-number-map* "1234567890")
 (setf *frame-number-map* "1234567890abcdefghijklmnopqrstuvwxyz")
@@ -57,57 +67,71 @@
 (setf *window-border-style* :thin)
 (set-focus-color "#999")
 
+; (set-font "-xos4-terminus-medium-r-normal--14-140-72-72-c-80-iso8859-15")
+
 ;; Commands and functions
 
-(defcommand battery-status () ()
-  (echo (run-shell-command "apm" t)))
+(defun chomp (x)
+  (remove #\newline x))
 
 (defun current-frame ()
   (tile-group-current-frame (current-group)))
+
+(defcommand (move-with-window tile-group) (dir) ((:direction "Direction: "))
+  (move-focus-and-or-window dir t))
+
+(defcommand battery-status () ()
+  (echo (run-shell-command "apm" t)))
 
 (defcommand (fprevious tile-group) () ()
   (focus-prev-frame (current-group)))
 
 (defun check-battery ()
-  (let ((string (remove #\newline (run-shell-command "apm -l" t))))
+  (let ((string (chomp (run-shell-command "apm -l" t))))
     (if (<= (parse-integer string) 10)
         (format nil "^1^b~a%^^*" string)
       (format nil "~a%" string))))
 
-;; Message window font
-; (set-font "-xos4-terminus-medium-r-normal--14-140-72-72-c-80-iso8859-15")
+(defun check-lang ()
+  (chomp (run-shell-command "setxkbmap -query | awk '/^layout:/ {print $2}'" t)))
 
-;;; Define window placement policy...
+(defcommand setlang (lang) (:string)
+  (run-shell-command (format nil "setlang ~a" lang))
+  (echo lang))
 
-; ;; Clear rules
-; (clear-window-placement-rules)
-; 
-; ;; Last rule to match takes precedence!
-; ;; TIP: if the argument to :title or :role begins with an ellipsis, a substring
-; ;; match is performed.
-; ;; TIP: if the :create flag is set then a missing group will be created and
-; ;; restored from *data-dir*/create file.
-; ;; TIP: if the :restore flag is set then group dump is restored even for an
-; ;; existing group using *data-dir*/restore file.
-; (define-frame-preference "Default"
-;   ;; frame raise lock (lock AND raise == jumpto)
-;   (0 t nil :class "Konqueror" :role "...konqueror-mainwindow")
-;   (1 t nil :class "XTerm"))
-; 
-; (define-frame-preference "Ardour"
-;   (0 t   t   :instance "ardour_editor" :type :normal)
-;   (0 t   t   :title "Ardour - Session Control")
-;   (0 nil nil :class "XTerm")
-;   (1 t   nil :type :normal)
-;   (1 t   t   :instance "ardour_mixer")
-;   (2 t   t   :instance "jvmetro")
-;   (1 t   t   :instance "qjackctl")
-;   (3 t   t   :instance "qjackctl" :role "qjackctlMainForm"))
-; 
-; (define-frame-preference "Shareland"
-;   (0 t   nil :class "XTerm")
-;   (1 nil t   :class "aMule"))
-; 
-; (define-frame-preference "Emacs"
-;   (1 t t :restore "emacs-editing-dump" :title "...xdvi")
-;   (0 t t :create "emacs-dump" :class "Emacs"))
+(setf *autosplit-next* nil)
+
+(defcommand autosplit-next () ()
+  (setf *autosplit-next* t))
+
+; TODO: not working!
+(defun autosplit-hook (window)
+ (when *autosplit-next*
+   (setf *autosplit-next* nil)
+   (let* ((old-window (current-window))
+          (group (current-group))
+          (old-frame-no (frame-number (tile-group-current-frame group)))
+          (new-frame-no (split-frame group :row)))
+      (pull-window (current-window) (frame-by-number group new-frame-no))
+      (pull-window old-window (frame-by-number group old-frame-no))
+      (focus-frame group (frame-by-number group new-frame-no)))))
+
+(defun automovemouse-hook (current-window last-window)
+  (when *ongoing-command*
+    (run-shell-command "xdotool mousemove --window $(xdotool getwindowfocus) --polar 0 0" nil)))
+
+;; Hooks
+
+(remove-all-hooks *new-window-hook*)
+(add-hook *new-window-hook* #'autosplit-hook)
+
+(setf *ongoing-command* nil)
+
+(remove-all-hooks *pre-command-hook*)
+(add-hook *pre-command-hook* (lambda (c) (setf *ongoing-command* t)))
+
+(remove-all-hooks *post-command-hook*)
+(add-hook *post-command-hook* (lambda (c) (setf *ongoing-command* nil)))
+
+(remove-all-hooks *focus-window-hook*)
+(add-hook *focus-window-hook* #'automovemouse-hook)
